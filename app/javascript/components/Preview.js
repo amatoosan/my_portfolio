306dd34e7eaeabad621eaebae2c79bc963f6da7e
“amatoosan” <amatoosan9525@gmail.com>
2020-12-18 00:53:01 +0900

    “画像プレビュー実装”

---
+++ app/javascript/components/Preview.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import DefaultPicture from '../../assets/images/images.png';
+
+const createObjectURL = (window.URL || window.webkitURL).createObjectURL || window.createObjectURL;
+
+class Preview extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      //default_image: '../assets/images/images.png'
+      image_src: '',
+    };
+  }
+
+  handleChangeFile(event) {
+    // ①イベントからfileの配列を受け取る
+    const files = event.target.files;
+    // ②createObjectURLで、files[0]を読み込む
+    const image_url = createObjectURL(files[0]);
+    // ③setStateする！
+    this.setState({image_src: image_url});
+  }
+
+
+  render(){
+    let preview;
+
+    if (this.state.image_src === '') {
+    preview = (
+      <div>
+        <input type="file" ref="file" onChange={(event) => {this.handleChangeFile(event)}} />
+        <img src={DefaultPicture} />
+      </div>
+    );
+    } else {
+    preview = (
+      <div>
+        <input type="file" ref="file" onChange={(event) => {this.handleChangeFile(event)}} />
+        <img src={this.state.image_src} />
+      </div>
+    );
+    }
+
+
+    return(
+      <div>
+        {preview}
+      </div>
+    );
+  }
+}
+export default Preview;
