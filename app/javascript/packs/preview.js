59a0b31bfa2338fcbe9150c87a8c33d6471cd9ca
“amatoosan” <amatoosan9525@gmail.com>
2021-01-13 23:23:10 +0900

    “ユーザー作成の画像プレビュー実装”

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ImageList = document.getElementById('post-image-prev');
+
+  document.getElementById('picture-select-btn').addEventListener('change', function(e){
+		// 画像が表示されている場合のみ、すでに存在している画像を削除する
+		const imageContent = document.querySelector('img');
+		if (imageContent){
+			imageContent.remove();
+		}
+		const file = e.target.files[0];
+		const blob = window.URL.createObjectURL(file);
+		// 画像を表示するためのdiv要素を生成
+		const imageElement = document.createElement('div');
+		// 表示する画像要素を生成し、画像を入れ込む
+		const blobImage = document.createElement('img');
+		blobImage.setAttribute('src', blob);
+		// 生成したHTMLの要素をブラウザに表示させる
+		imageElement.appendChild(blobImage);
+		ImageList.appendChild(imageElement);
+	});
+});
