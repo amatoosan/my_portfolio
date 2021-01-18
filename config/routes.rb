1f1066759336518d3635921df667ed70cf09813f
“amatoosan” <amatoosan9525@gmail.com>
2021-01-18 23:09:14 +0900

    “投稿のアクセス制御まで”

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
   root 'homes#top'
 
   #react練習用
-  get '/test',  to: 'homes#test'
-  get '/test.json',  to: 'homes#test'
+  get '/test',      to: 'homes#test'
+  get '/test.json', to: 'homes#test'
 
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :questions, only: [:create, :destroy]
 
 end
